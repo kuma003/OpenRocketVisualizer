9f810cabe44dbe9de3d58ba01ba70b88f1ea64f9
Kuma <sato.kuma.t7@dc.tohoku.ac.jp>
2024-12-06 19:32:50 +0900

    update font managiment class

--- src/fonts.py
+++ src/fonts.py
@@ -1,25 +1,66 @@
+"""font."""
 import requests
 import os
+import pygame as pg
 
-def download_font(url, save_path):
-    response = requests.get(url)
-    if response.status_code == 200:
-        os.makedirs("./fonts", exist_ok=True)
-        with open(save_path, 'wb') as file:
-            file.write(response.content)
-        print(f"Downloaded {save_path}")
-    else:
-        print(f"Failed to download {url}")
+class Fonts:
+    """
+        Font manager class. This class manages the fonts used in the game.
 
-def main():
-    fonts = {
-        "Roboto": "https://github.com/google/fonts/raw/main/apache/robotomono/RobotoMono-Italic%5Bwght%5D.ttf",
-        # "OpenSans": "https://github.com/google/fonts/raw/main/apache/opensans/OpenSans-Regular.ttf"
+        Attributes:
+            font_list (dict[str, str]): the dictionary of fonts that can be used in the game. The key is the name of the font, and the value is the github URL of the font file. you can add more fonts to the font_list by adding the font name and the URL of the font file.
+    """
+    font_list = {
+        "ZenMaruGothic": "https://github.com/googlefonts/zen-marugothic/blob/main/fonts/ttf/ZenMaruGothic-Regular.ttf",
     }
+    
+    __fonts : dict[str, pg.font.Font] = {}
+    
+    @classmethod
+    def initialize(cls) -> None:
+        """
+            Initialize the fonts. This method should be called before using the fonts.
+        """
+        pg.font.init() 
+    
+    @classmethod
+    def get_font(cls, font_name : str):
+        """
+            Get the font object by the font name.
 
-    for font_name, font_url in fonts.items():
-        save_path = f"./fonts/{font_name}.ttf"
-        download_font(font_url, save_path)
+            Args:
+                font_name (str): the name of the font.
+
+            Returns:
+                pg.font.Font: the font object.
+        """
+        return cls.__fonts.get(font_name)
+    
+    @classmethod
+    def download_fonts(cls):
+        """
+            Download the fonts. This method downloads the fonts from the internet and saves them in the "./fonts" directory.
+        """
+        os.makedirs("./fonts", exist_ok=True)
+        for font_name, font_url in cls.font_list.items():
+            if font_name in cls.__fonts:
+                # no need to download the font if it's already downloaded
+                continue
+            
+            # download font
+            response = requests.get(font_url)
+            save_path = os.path.join("fonts", f"{font_name}.ttf")
+            with open(save_path, 'wb') as file:
+                if response.status_code == 200:
+                    file.write(response.content)
+                    print(f"Downloaded {font_name}")
+                else:
+                    print(f"Failed to download {font_name}")
+                    continue
+            cls.__fonts[font_name] = pg.font.Font(save_path, 32)
+
+Fonts.initialize() # initialize the fonts
 
 if __name__ == "__main__":
-    main()
+    Fonts.download_fonts()
+    print(Fonts.get_font("ZenMaruGothic"))

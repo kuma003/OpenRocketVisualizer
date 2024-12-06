9353b7d82995e18150f605c42933fff79995ac52
kuma-1220 <sato.kuma.t7@dc.tohoku.ac.jp>
2024-12-06 09:40:34 +0900

    Add scene management and initial Top scene implementation

--- src/scene.py
+++ src/scene.py
@@ -1,5 +1,14 @@
 import pygame as pg
+import numpy as np
 from abc import ABCMeta, abstractmethod
+import enum
+
+class SCENE_STATE(enum.Enum):
+    TOP = 0
+    BRIEFING = 1
+    GAME = 2
+    EXIT = 3
+    QUIT = 4
 
 class AppMain:
     def __init__(self) -> None:
@@ -10,77 +19,63 @@ class AppMain:
         # self.screen = pg.display.set_mode((screen_width, screen_height), pg.FULLSCREEN)
         self.screen = pg.display.set_mode((screen_width//2, screen_height//2))
         
-        self.scenes : list[Scene] = []
-        self.sceneIds : list[int] = []
+        self.top = Top()
+                
+        self.scene_flag = SCENE_STATE.TOP
     
-    def register_scene(self, scene: Scene):
-        self.scenes.append(Scene)
-        self.sceneIds.append(len(self.scenes) - 1)
-        return self.sceneIds[-1]
-    
-    def switch_scene(self, id : int):
-        self.scenes[id].enable = True;
+    def run(self) -> None:
+        clock = pg.time.Clock()
         
+        fps = 60
+        clock.tick(fps)
+        while True:
+            # main loop
+            if self.scene_flag is SCENE_STATE.TOP:
+                self.scene_flag = self.top.exec(self.screen)
+            else:
+                break
+            
     
     
 
-class Scene(metaclass = ABCMeta):
-    def __init__(self, enable=False) -> None:
-        self.enable = False
-        self.nextId
+class Top:
+    """
+        Top scene of the game. This Scene is the first scene to be displayed when the game starts.
+    """
+    COLOR_BACKGROUND = pg.Color(0xFA, 0xFC, 0xFF) # background color of the scene.
+    ICON_FTE = pg.image.load( "img/FTE.png") # F.T.E. icon image.
     
-    # def update(self) -> None:
-    #     """
-    #         Update function that is called every frame unless `self.enable` is set to True.
-            
-    #         This function is not intended to be overriden by subclass. Custom behavior should  
-    #         be implemented in the `_update` function instead.
-    #     """
-    #     if self.enable:
-    #         self._update()
-    
-    # def _update(self) -> None:
-    #     """
-    #         Custom behavior function called as part of the update process.
-
-    #         Subclasses can override this method to implement specific behavior that  
-    #         should occur during each frame update. This function is called only when  
-    #         `self.enable` is set to True.
-            
-    #         Note: 
-    #             - `_update` is designed for subclass-specific logic and should not call 
-    #             the parent class's `update` method directly.
-    #     """
-    #     pass
-    
-    # def draw(self, screen : pg.Surface) -> None:
-    #     """
-    #         Drawing function that is called every frame unless `self.enable` is set to True.
-            
-    #         This function is not intended to be overriden by subclass. Custom behavior should  
-    #         be implemented in the `_draw` function instead.
-            
-    #         Args:
-    #             - screen : A Pygame surface used for rendering graphics.
-    #     """
-    #     if self.enable:
-    #         self._draw(scene=screen)
-    
-    # @abstractmethod
-    # def _draw(self, scene) -> None:
-    #     """
-    #         Custom behavior function called as part of the drawing process.
-
-    #         Subclasses can override this method to implement specific behavior that  
-    #         should occur during each frame update. This function is called only when  
-    #         `self.enable` is set to True.
-            
-    #         Note: 
-    #             - `_draw` is designed for subclass-specific logic and should not call 
-    #             the parent class's `draw` method directly.
-    #     """
-        pass
-
-class Top(Scene):
-    def __init__(self):
+    def __init__(self) -> None:
+        fte_icon_arr = pg.surfarray.pixels3d(Top.ICON_FTE)
+        fte_icon_alpha = pg.surfarray.pixels_alpha(Top.ICON_FTE)
+        mask = fte_icon_alpha > 0
+        fte_icon_arr[mask] = (0, 0, 0)
         
+        self.ICON_FTE = pg.Surface(Top.ICON_FTE.get_size(), pg.SRCALPHA)
+        pg.surfarray.blit_array(self.ICON_FTE, fte_icon_arr)
+        
+        self.ICON_FTE = self.ICON_FTE.convert_alpha()
+        pg.surfarray.pixels_alpha(self.ICON_FTE)[:] = 32
+            
+    def exec(self, scene: pg.surface) -> None:
+        """
+            Execute the scene.
+        """
+        
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return SCENE_STATE.QUIT
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    return SCENE_STATE.QUIT
+        
+        screen_width, screen_height = scene.get_width(), scene.get_height()
+        if screen_height > screen_width:
+            fte_icon = pg.transform.scale(Top.ICON_FTE, (screen_width, screen_width))
+        else:
+            fte_icon = pg.transform.scale(Top.ICON_FTE, (screen_height, screen_height))
+        
+        scene.fill(Top.COLOR_BACKGROUND)
+        scene.blit(fte_icon, (0 if screen_height > screen_width else (screen_width - screen_height)//2 , 0 if screen_width > screen_height else (screen_height - screen_width)//2))
+        pg.display.update()
+        return SCENE_STATE.TOP

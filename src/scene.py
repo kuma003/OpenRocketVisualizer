6a0b8f0b77ebf416f038b408208a5a2cf78cf164
Kuma <sato.kuma.t7@dc.tohoku.ac.jp>
2024-12-21 16:44:01 +0900

    add loading transparent image func

--- src/scene.py
+++ src/scene.py
@@ -3,6 +3,7 @@ import pygame as pg
 import numpy as np
 from abc import ABCMeta, abstractmethod
 import enum
+from src.fonts import Fonts
 
 class SCENE_STATE(enum.Enum):
     """
@@ -22,7 +23,8 @@ class AppMain:
 
         # self.screen = pg.display.set_mode((screen_width, screen_height), pg.FULLSCREEN)
         self.screen = pg.display.set_mode((screen_width//2, screen_height//2))
-        
+        pg.display.set_caption("F.T.E. OpenRocket Flight Visualizer")
+        pg.display.set_icon(pg.image.load("img/ろけにゃん_ロケット.png"))        
         self.top = Top()
                 
         self.scene_flag = SCENE_STATE.TOP
@@ -38,9 +40,23 @@ class AppMain:
                 self.scene_flag = self.top.exec(self.screen)
             else:
                 break
-            
-    
-    
+
+def load_transparent_img(filename : str, filled_color : pg.color):
+    img = pg.image.load(filename)
+
+    img_arr = pg.surfarray.pixels3d(img)
+    img_alpha = pg.surfarray.pixels_alpha(img)
+    mask = img_alpha > 0
+    img_arr[mask] = filled_color[:3]
+
+    filled_img = pg.Surface(img.get_size(), pg.SRCALPHA)
+    pg.surfarray.blit_array(filled_img, img_arr)
+
+    # アルファチャンネルを設定
+    alpha_arr = pg.surfarray.pixels_alpha(filled_img)
+    alpha_arr[mask] = img_alpha[mask]
+
+    return filled_img
 
 class Top:
     """
@@ -48,22 +64,27 @@ class Top:
     """
     COLOR_BACKGROUND = pg.Color(0xFA, 0xFC, 0xFF) # background color of the scene.
     COLOR_ICON  = pg.Color(0xD0, 0xD0, 0xD0) # color of the icon.
-    ICON_FTE = pg.image.load( "img/FTE.png") # F.T.E. icon image.
+    # ICON_FTE = load_transparent_img("img/FTE.png", COLOR_ICON) # F.T.E. icon image.
+    ICON_FTE = None
+    COPYRIGHT_TEXT = Fonts.get_font("ZenMaruGothic", 15).render("@2024 From The Earth.", True, pg.Color(0xC0, 0xC0, 0xC0))
     
     def __init__(self) -> None:
-        fte_icon_arr = pg.surfarray.pixels3d(Top.ICON_FTE)
-        fte_icon_alpha = pg.surfarray.pixels_alpha(Top.ICON_FTE)
-        mask = fte_icon_alpha > 0
-        fte_icon_arr[mask] = self.COLOR_ICON[:3]
+        # self.ICON_FTE = fill_image()
+        # fte_icon_arr = pg.surfarray.pixels3d(Top.ICON_FTE)
+        # fte_icon_alpha = pg.surfarray.pixels_alpha(Top.ICON_FTE)
+        # mask = fte_icon_alpha > 0
+        # fte_icon_arr[mask] = self.COLOR_ICON[:3]
         
-        self.ICON_FTE = pg.Surface(Top.ICON_FTE.get_size(), pg.SRCALPHA)
-        pg.surfarray.blit_array(self.ICON_FTE, fte_icon_arr)
-            
+        # self.ICON_FTE = pg.Surface(Top.ICON_FTE.get_size(), pg.SRCALPHA)
+        # pg.surfarray.blit_array(self.ICON_FTE, fte_icon_arr)
+        # self.ICON_FTE = load_transparent_img("img/FTE.png", self.COLOR_ICON) 
+        Top.ICON_FTE = load_transparent_img("img/FTE.png", self.COLOR_ICON)
+        pass
+
     def exec(self, scene: pg.surface) -> None:
         """
             Execute the scene.
         """
-        
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return SCENE_STATE.QUIT
@@ -80,5 +101,6 @@ class Top:
         scene.fill(Top.COLOR_BACKGROUND)
         scene.blit(fte_icon, (0 if screen_height > screen_width else (screen_width - screen_height)//2 ,
                               0 if screen_width > screen_height else (screen_height - screen_width)//2))
+        scene.blit(self.COPYRIGHT_TEXT, (screen_width - self.COPYRIGHT_TEXT.get_width(), screen_height - self.COPYRIGHT_TEXT.get_height()))
         pg.display.update()
         return SCENE_STATE.TOP

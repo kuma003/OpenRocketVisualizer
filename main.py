f7bf33c2f58ecfaeb7e6f7739f9967545043139f
kuma-1220 <sato.kuma.t7@dc.tohoku.ac.jp>
2024-12-22 17:33:35 +0900

    remove unused comments

--- main.py
+++ main.py
@@ -2,45 +2,6 @@ import pygame as pg
 import orhelper
 import src.scene as scene
 
-# class AppMain:
-#     def __init__(self) -> None:
-#         pg.init()
-#         info = pg.display.Info()
-#         screen_width, screen_height = info.current_w, info.current_h 
-
-#         # self.screen = pg.display.set_mode((screen_width, screen_height), pg.FULLSCREEN)
-#         self.screen = pg.display.set_mode((screen_width//2, screen_height//2))
-
-#     def update(self) -> None:
-#         pass # 空実装
-    
-#     def draw(self) -> None:
-#         self.screen.fill(pg.Color("blue"))
-#         pg.display.update()
-    
-    
-#     def run(self) -> None:
-#         clock = pg.time.Clock()
-        
-#         while True:
-#             # main loop
-#             fps = 60
-#             clock.tick(fps)
-            
-#             should_quit = False
-            
-#             for event in pg.event.get():
-#                 if event.type == pg.QUIT:
-#                     should_quit = True
-#                 if event.type == pg.KEYDOWN:
-#                     if event.key == pg.K_ESCAPE:
-#                         should_quit = True
-            
-#             if should_quit:
-#                 break
-#             self.update()
-#             self.draw()
-
 if __name__ == "__main__":
     app = scene.AppMain()
     app.run()

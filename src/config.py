3d5b50e9e839dfa428a398c4e4f8aa7f962d410e
kuma-1220 <sato.kuma.t7@dc.tohoku.ac.jp>
2024-12-23 09:24:12 +0900

    add config file

---
+++ src/config.py
@@ -0,0 +1,8 @@
+import pygame as pg
+
+COLOR_PALE_WHITE    = pg.Color(0xFC, 0xFC, 0xFF) 
+COLOR_PALE_GRAY     = pg.Color(0xD0, 0xD0, 0xD0)
+COLOR_GRAY1         = pg.Color(0xC0, 0xC0, 0xC0)
+COLOR_GRAY2         = pg.Color(0x80, 0x80, 0x80)
+
+TEXT_COPYRIGHT = "@2024 From The Earth."

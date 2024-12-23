5498c45a6612d4dd282f93947f2c2f3e83318041
Kuma <sato.kuma.t7@dc.tohoku.ac.jp>
2024-12-24 07:28:20 +0900

    update configs

--- src/config.py
+++ src/config.py
@@ -1,8 +1,9 @@
 import pygame as pg
 
-COLOR_PALE_WHITE    = pg.Color(0xFC, 0xFC, 0xFF) 
-COLOR_PALE_GRAY     = pg.Color(0xD0, 0xD0, 0xD0)
+COLOR_PALE_WHITE1   = pg.Color(0xFC, 0xFC, 0xFF)
+COLOR_PALE_GRAY     = pg.Color(0xF0, 0xF0, 0xF0)
 COLOR_GRAY1         = pg.Color(0xC0, 0xC0, 0xC0)
 COLOR_GRAY2         = pg.Color(0x80, 0x80, 0x80)
+COLOR_BLACK         = pg.Color(0x50, 0x50, 0x50)
 
 TEXT_COPYRIGHT = "@2024 From The Earth."

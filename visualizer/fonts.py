97a703e76bcda6d6472259e4e6d2e264f790f24f
kuma-1220 <sato.kuma.t7@dc.tohoku.ac.jp>
2025-01-22 20:59:41 +0900

    refactor: improve code formatting

--- visualizer/fonts.py
+++ visualizer/fonts.py
@@ -1,51 +1,58 @@
 """font.py"""
+
 import requests
 import os
 import pygame as pg
 
+
 class Fonts:
     """
-        Font manager class.
+    Font manager class.
 
-        Attributes:
-            font_list (dict[str, str]): the dictionary of fonts that can be used in the game. The key is the name of the font, and the value is the github URL of the font file. you can add more fonts to the font_list by adding the font name and the URL of the font file.
+    Attributes:
+        font_list (dict[str, str]): the dictionary of fonts that can be used in the game. The key is the name of the font, and the value is the github URL of the font file. you can add more fonts to the font_list by adding the font name and the URL of the font file.
     """
+
     font_list = {
         "ZenKakuGothic": "https://github.com/googlefonts/zen-kakugothic/raw/refs/heads/main/fonts/ttf/ZenKakuGothicAntique-Medium.ttf",
-        "oswald": "https://github.com/vernnobile/OswaldFont/raw/refs/heads/master/3.0/Roman/400/Oswald-Regular.ttf"
+        "oswald": "https://github.com/vernnobile/OswaldFont/raw/refs/heads/master/3.0/Roman/400/Oswald-Regular.ttf",
     }
-    
+
     __FONT_DIR = "./fonts"
-    
+
     @classmethod
     def initialize(cls) -> None:
         """
-            Initialize the fonts. This method should be called before using the fonts.
+        Initialize the fonts. This method should be called before using the fonts.
         """
-        pg.font.init() 
-    
+        pg.font.init()
+
     @classmethod
-    def get_font(cls, font_name : str, font_size : int = 32) -> pg.font.Font:
+    def get_font(cls, font_name: str, font_size: int = 32) -> pg.font.Font:
         """
-            Get the font object by the font name.
+        Get the font object by the font name.
 
-            Args:
-                font_name (str): the name of the font.
-                font_size (int): the size of the font.
+        Args:
+            font_name (str): the name of the font.
+            font_size (int): the size of the font.
 
-            Returns:
-                pg.font.Font: the font object.
+        Returns:
+            pg.font.Font: the font object.
         """
         if os.path.exists(os.path.join(cls.__FONT_DIR, f"{font_name}.ttf")):
-            return pg.font.Font(os.path.join(cls.__FONT_DIR, f"{font_name}.ttf"), font_size)
+            return pg.font.Font(
+                os.path.join(cls.__FONT_DIR, f"{font_name}.ttf"), font_size
+            )
         else:
-            print(f"Font {font_name} is not found. Make sure you have downloaded the font via 'download_fonts'.")
+            print(
+                f"Font {font_name} is not found. Make sure you have downloaded the font via 'download_fonts'."
+            )
             return None
-    
+
     @classmethod
     def download_fonts(cls):
         """
-            Download the fonts. This method downloads the fonts from the internet and saves them in the "./fonts" directory.
+        Download the fonts. This method downloads the fonts from the internet and saves them in the "./fonts" directory.
         """
         print("Verifying fonts...")
         os.makedirs(cls.__FONT_DIR, exist_ok=True)
@@ -58,7 +65,7 @@ class Fonts:
             # download font
             response = requests.get(font_url)
             save_path = os.path.join("fonts", f"{font_name}.ttf")
-            with open(save_path, 'wb') as file:
+            with open(save_path, "wb") as file:
                 if response.status_code == 200:
                     file.write(response.content)
                     print("Done.")
@@ -67,8 +74,9 @@ class Fonts:
                     continue
         print("All fonts are ready.")
 
-Fonts.initialize() # initialize the fonts
-Fonts.download_fonts() # download the fonts
+
+Fonts.initialize()  # initialize the fonts
+Fonts.download_fonts()  # download the fonts
 
 if __name__ == "__main__":
     print(Fonts.get_font("ZenMaruGothic"))

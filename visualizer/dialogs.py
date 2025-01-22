5725b5af9cc0b101926aedc448250325c240b3f8
kuma-1220 <sato.kuma.t7@dc.tohoku.ac.jp>
2025-01-22 20:59:12 +0900

    feat: add file dialog functions for opening OpenRocket files and confirming exit

---
+++ visualizer/dialogs.py
@@ -0,0 +1,30 @@
+"""dialogs.py"""
+
+import tkinter
+import tkinter.filedialog
+import pygame as pg
+
+
+def open_ork_file():
+    """Open file dialog and return the file name"""
+    top = tkinter.Tk()
+    top.withdraw()  # hide window
+    file_name = tkinter.filedialog.askopenfilename(
+        parent=top, filetypes=[("OpenRocket Files", "*.ork")]
+    )
+    top.destroy()
+    # NOTE: Clear event queue to avoid double file open event occurred(ad hoc). That is why other event (e.g. QUIT) does not work.
+    pg.event.clear()
+    print(f"Open file: {file_name}")
+    return file_name
+
+
+def ask_whether_to_exit():
+    """Ask whether to exit the application"""
+    top = tkinter.Tk()
+    top.withdraw()  # hide window
+    res = tkinter.messagebox.askyesno("Exit", "Do you want to exit?")
+    top.destroy()
+    # NOTE: Clear event queue to avoid double file open event occurred(ad hoc). That is why other event (e.g. QUIT) does not work.
+    pg.event.clear()
+    return res
